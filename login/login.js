e0601eb90bc52e156570e1ff57b59be7db02f571
Rohit7953-sudo <sanroh7953@gmail.com>
2026-01-13 16:41:19 +0530

    login page

---
+++ login/login.js
@@ -0,0 +1,18 @@
+const loginForm = document.getElementById("loginForm");
+const errorMsg = document.getElementById("errorMsg");
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  // Simple authentication
+  if (username === "admin" && password === "1234") {
+    // store session flag
+    localStorage.setItem("isAdminLoggedIn", "true");
+    window.open("../dashboard/dash.html", "_blank");
+  } else {
+    errorMsg.textContent = "Invalid username or password!";
+  }
+});
